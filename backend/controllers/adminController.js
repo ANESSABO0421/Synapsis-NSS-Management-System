5fc4010d34488a8805f48d09437856a4e4c79613
Anees <aneesaboo123@gmail.com>
2025-09-19 12:09:16 +0530

    added:nodemailer

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,17 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import nodemailer from "nodemailer";
+
+// sending otp using nodemailer
+const sendOTPEmail = async (email, otp) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
+  });
+  await transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: "Your NSS Admin OTP",
+    text: `Your OTP is ${otp}. Valid for 5 minutes.`,
+  });
+};
