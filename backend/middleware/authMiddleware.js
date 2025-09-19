605b92d7c496750e2a9511a1a2057d1cc0288ec5
Anees <aneesaboo123@gmail.com>
2025-09-19 12:29:56 +0530

    added:auth middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import Admin from "../models/Admin.js";
+
+export const protect = async (req, res, next) => {
+  let token;
+  //token starts with bearer
+  if (req.headers.authorization && req.authorization.startsWith("Bearer")) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      //check both the jwt code and token then verify
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      //take user from db and exclude password
+      req.admin = await Admin.findById(decoded.id).select("-password");
+      next();
+    } catch (error) {
+      return res.status(401).json({ message: "Not authorized ,token failed" });
+    }
+  }
+
+  if (!token) {
+    return res.status(401).json({ message: "Not authorised and no token" });
+  }
+};
