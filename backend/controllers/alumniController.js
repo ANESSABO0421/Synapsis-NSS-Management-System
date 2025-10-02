3c53214f59882b7a43c9221b9e44445679875d9d
Anees <aneesaboo123@gmail.com>
2025-10-02 17:24:26 +0530

    finished:alumni controller

--- backend/controllers/alumniController.js
+++ backend/controllers/alumniController.js
@@ -143,7 +143,75 @@ export const approveAlumni = async (req, res) => {
 // update alumni
 export const updateAlumni = async (req, res) => {
   try {
-    const { name } = req.body;
+    const { id } = req.params;
+    const updates = req.body;
+
+    if (updates.password)
+      updates.password = await bcrypt.hash(updates.password, 10);
+
+    const alumni = await Alumni.findByIdAndUpdate(id, updates, {
+      new: true,
+      runValidators: true,
+    }).select("-password");
+    if (!alumni)
+      return res
+        .status(404)
+        .json({ success: false, message: "Alumni not found" });
+
+    res.json({ success: true, message: "Alumni updated", alumni });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+};
+
+// delete alumni
+export const deleteAlumni = async (req, res) => {
+  try {
+    const alumni = await Alumni.findByIdAndDelete(req.params.id);
+    if (!alumni) {
+      return res
+        .status(404)
+        .jsn({ success: false, message: "Alumni not found" });
+    }
+    res.json({ success: true, message: "Alumni deleted successfully!!!" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+// add testimonial
+export const addTestimonial = async (req, res) => {
+  try {
+    const alumni = await Alumni.findById(req.params.id);
+    if (!alumni) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Alumni not found" });
+    }
+    alumni.testimonials.push({ message: req.body.message });
+    await alumni.save();
+    res.json({ success: true, message: "testimonial submitted" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+// add achievements
+export const addAchievement = async (req, res) => {
+  try {
+    const alumni = await Alumni.findById(req.params.id);
+    if (!alumni) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Alumni not found" });
+    }
+    alumni.achievements.push({
+      title: req.body.title,
+      description: req.body.description,
+    });
+
+    await alumni.save();
+    res.json({ success: true, message: "Achievement added" });
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
   }
